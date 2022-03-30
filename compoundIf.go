b475f2f2bd10509798ee637fdeeba7b510b6f728
jpschew <jps.chew@gmail.com>
2022-03-30 13:08:10 +0800

    added code to determine leap year

--- compoundIf.go
+++ compoundIf.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	//Insert your code here
+	var year string
+	fmt.Println("Enter a year: ")
+	fmt.Scanln(&year)
+
+	if year, _ := strconv.ParseInt(year, 10, 0); year%400 == 0 {
+		fmt.Println("It is a leap year")
+	} else if year%4 == 0 {
+		fmt.Println("It is a leap year")
+	} else {
+		fmt.Println("It is not a leap year")
+	}
 }
